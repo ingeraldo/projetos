bf6153b929dc11835a9bdd3db28d58939e8ec8e4
Isaac Neves Geraldo <121636511+ingeraldo@users.noreply.github.com>
2023-02-27 13:38:12 -0300

    Update openai-R-chatbot.R

--- openai-R-chatbot.R
+++ openai-R-chatbot.R
@@ -1,8 +1,12 @@
+## INSTALAR PACOTE 'openai' - disponível apenas para versões acima da 4.2
 #install.packages("openai")
 library("openai")
 shell('cls')
+
+## SETAR A API KEY (crie uma conta no site da openai para gerar uma API Key)
 Sys.setenv(OPENAI_API_KEY = "sk-...")
 
+## FAZER A INSTRUÇÃO E REQUISIÇÃO
 requisicao_completa <- 'Você é um consultor de musculação. Responda às perguntas educadamente. Dê respostas pequenas, mas informativas. Responda como "CONSULTOR: "'
 
 message(requisicao_completa)
@@ -10,7 +14,7 @@ message(requisicao_completa)
 while (TRUE) {
   requisicao_atual <- paste0(requisicao_completa,"\n\nVOCÊ: ",readline(prompt = "\nVOCÊ: "))
   message("\nO robô está digitando...")
-  result <- create_completion(
+  resultado <- create_completion(
     model = "text-davinci-003",
     prompt = requisicao_atual,
     temperature = 0.9,
@@ -20,6 +24,6 @@ while (TRUE) {
     presence_penalty = 0.6
   )
   shell("cls")
-  requisicao_completa <- paste0(requisicao_atual,result$choices$text)
+  requisicao_completa <- paste0(requisicao_atual,resultado$choices$text)
   message(requisicao_completa)
 }
